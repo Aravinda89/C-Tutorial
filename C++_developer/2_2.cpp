d74bb932baaba7cab5b541300cdaeeb5ec3c944b
Aravinda89 <1513223865@qq.com>
2020-11-27 23:06:08 +0800

    Update 2_2.cpp

--- C++_developer/2_2.cpp
+++ C++_developer/2_2.cpp
@@ -14,18 +14,24 @@ int main(){
   bool gameOver = false;
   int chipsInPile = 0;
   int chipsTaken = 0;
+  int maxPerTurn = 0;
 
   string playerName[2];
   cout << "Player 1 pls Enter your name:" << endl;
   cin >> playerName[0];
   cout << "Player 2 pls Enter your name:" << endl;
   cin >> playerName[1];
+  cout << "Thanks and GoodLuck.";
 
   srand(time(0));
 
   chipsInPile = (rand() % MAX_CHIPS) + 1;
-  cout << "This round will start with " << chipsInPile << endl;
-  cout << "You can only take " << static_cast<int>(chipsInPile * MAX_TURN) << endl;
+  cout << "This round will start with " << chipsInPile << " chips in pile."<<endl;
+  // cout << "You can only take " << static_cast<int>(chipsInPile * MAX_TURN) << endl;
+  maxPerTurn = (chipsInPile * MAX_TURN);
+  cout << "You can take up to " << maxPerTurn << endl;
+  chipsTaken = (rand() % maxPerTurn) + 1;
+  cout << "Number Taken: " << chipsTaken << endl;
 
   return 0;
 }
