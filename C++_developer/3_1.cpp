d99b16a33a7d6757f784d274bebda5a7cb5170d8
Aravinda89 <1513223865@qq.com>
2020-11-29 16:58:49 +0800

    if

--- C++_developer/3_1.cpp
+++ C++_developer/3_1.cpp
@@ -1,5 +1,4 @@
 //game
-
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -14,11 +13,11 @@ int main(){
   bool gameOver = false;
   int chipsInPile = 0;
   int chipsTaken = 0;
-  int maxPerTurn = 0;
+
+  string playerName[2];
 
   srand(time(0));
 
-  string playerName[2];
   cout << "Player 1 pls Enter your name:" << endl;
   cin >> playerName[0];
   cout << "Player 2 pls Enter your name:" << endl;
@@ -27,11 +26,15 @@ int main(){
 
   chipsInPile = (rand() % MAX_CHIPS) + 1;
   cout << "This round will start with " << chipsInPile << " chips in pile."<<endl;
-  // cout << "You can only take " << static_cast<int>(chipsInPile * MAX_TURN) << endl;
-  maxPerTurn = (chipsInPile * MAX_TURN);
-  cout << "You can take up to " << maxPerTurn << endl;
-  chipsTaken = (rand() % maxPerTurn) + 1;
-  cout << "Number Taken: " << chipsTaken << endl;
+  if(player1Turn){
+    cout << playerName[0] << "How many chips would you like?\n";
+  }
+  else{
+    cout << playerName[1] << "How many chips would you like?\n";
+  }
+
+  cout << "You can only take " << static_cast<int>(chipsInPile * MAX_TURN) << endl;
+  cin >> chipsTaken;
 
   return 0;
 }
