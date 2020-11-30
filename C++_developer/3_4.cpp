ca6d42ff46a1d8111ef1fdc855e81ef24a41f6ff
Aravinda89 <1513223865@qq.com>
2020-11-30 23:51:16 +0800

    do while

---
+++ C++_developer/3_4.cpp
@@ -0,0 +1,22 @@
+// Do while
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int userChoice = 0;
+
+	do
+	{
+		cout << "Hello welcome\n";
+		cout << "Pls choose the options\n";
+		cout << "1. continue\n";
+		cout << "2. quit\n";
+    	cin >> userChoice;
+
+	} while(userChoice==1);
+
+	return 0;
+}
